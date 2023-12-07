b89027c33a4dd78855fa9c7aa21ddb1d261f4961
Nguyễn Phúc Hiệp <132082516+DraiGShyva@users.noreply.github.com>
2023-12-08 01:40:33 +0700

    Big update 08/12/23

--- main.py
+++ main.py
@@ -1,47 +1,67 @@
 from module.pose_landmarker import extract_pose_features, draw
-from module.init import capture, imshow, waitKey, destroyAllWindows, init_writer, init_csv, name_folder
+import pandas as pd
+from module.init import (
+    imshow,
+    waitKey,
+    destroyAllWindows,
+    init,
+)
 
-# Khởi tạo Webcam
-cap = capture()
-
-# Khởi tạo folder_name
-import os
-folder_name = name_folder("fall_detection/video/vid_") # Tên thư mục
-os.makedirs(folder_name) # Tạo thư mục
-
-# Khởi tạo video_writer
-video_writer = init_writer(cap, folder_name)
-
-# Khởi tạo csv_writer
-csv_writer = init_csv(folder_name+"/data.csv")
+cap, video, csv, model, folder_name = init()
 
 # Vòng lặp chính
-while cap.isOpened():
+counter = 0
 
+while True:
     # Đọc frame từ webcam
     frame = cap.read()[1]
 
     # Trích xuất các điểm mốc
     landmarks = extract_pose_features(frame)
 
-    # Nếu có điểm mốc thì vẽ các điểm mốc và các đường nối
+    list = []
+
+    # Nếu có điểm mốc thì vẽ các điểm mốc
     if landmarks is not None:
         frame = draw(frame.copy(), landmarks)
 
-        #chuyển landmarks thành mảng 1 chiều
+        # chuyển landmarks thành mảng 1 chiều
         list = [j for i in landmarks for j in i]
 
-        # Ghi dữ liệu xuống file csv
-        csv_writer.writerow(list)
+    # Ghi dữ liệu xuống file csv
+    csv.writerow(list)
+
+    # Đọc dữ liệu từ file csv
+    try:
+        df = pd.read_csv(folder_name + "/data.csv")
+    except pd.errors.EmptyDataError:
+        df = pd.DataFrame()
+
+    # Nếu dữ liệu lớn hơn 28 dòng
+    if counter == 10:
+        counter = 0
+        if len(df) > 28:
+            # Lấy 28 dòng cuối cùng và chuyển thành mảng 1 chiều
+            data_list = [[j for i in df.tail(28).values for j in i]]
+
+            # Dự đoán kết quả mỗi 8 frame
+
+            result = model.predict(data_list)
+
+            # Nếu kết quả là 1 thì hiển thị cảnh báo
+            if result == 1:
+                print("Fall Detected")
 
     # Hiển thị kết quả
-    imshow('Pose Detection', frame)
+    imshow("Pose Detection", frame)
 
     # Ghi frame xuống video
-    video_writer.write(frame)
+    video.write(frame)
 
     # Nhấn phím ESC để thoát
-    if waitKey(1) & 0xFF == 27: 
+    if waitKey(1) & 0xFF == 27:
         cap.release()
-        video_writer.release()
+        video.release()
         destroyAllWindows()
+
+    counter += 1
--- module/pose_landmarker.py
+++ module/pose_landmarker.py
@@ -1,19 +1,24 @@
-from cv2 import circle, COLOR_BGR2RGB, cvtColor
+from cv2 import circle, COLOR_BGR2RGB, cvtColor, line
 from mediapipe.python.solutions.pose import Pose
 
-mp_pose = Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5, model_complexity=1, static_image_mode=False)
+mp_pose = Pose(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5,
+    model_complexity=1,
+    static_image_mode=False,
+)
 
 # tạo list các điểm mốc cần thiết
 n_landmarks = [[11, 12], [23, 24], 27, 28]
 
+
 # Hàm trích xuất đặc trưng từ ảnh
 def extract_pose_features(frame):
-
     # chuyển ảnh sang RGB
     frame_rgb = cvtColor(frame, COLOR_BGR2RGB)
 
     # xử lý ảnh
-    results = mp_pose.process(frame_rgb).pose_landmarks # type: ignore
+    results = mp_pose.process(frame_rgb).pose_landmarks  # type: ignore
 
     # nếu không có kết quả thì trả về None
     if results is None:
@@ -38,13 +43,20 @@ def extract_pose_features(frame):
 
     return landmarks
 
+
 # Hàm vẽ các điểm mốc và các đường nối
 def draw(frame, landmarks):
     # lấy kích thước ảnh
     frame_height, frame_width, _ = frame.shape
-    
+
     # vẽ các điểm mốc
     for landmark in landmarks:
-        circle(frame, (int(landmark[0] * frame_width), int(landmark[1] * frame_height)), 5, (0, 0, 255), -1)
+        circle(
+            frame,
+            (int(landmark[0] * frame_width), int(landmark[1] * frame_height)),
+            5,
+            (0, 0, 255),
+            -1,
+        )
 
     return frame
